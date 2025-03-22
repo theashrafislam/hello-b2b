a0e05940c0aa7b9911bf6e20d0bbf78aebdca666
Ashraful Islam <theashrafislam@gmail.com>
2025-03-22 16:14:55 +0600

    hero section is complate

--- src/app/Components/Home/HeroSection.jsx
+++ src/app/Components/Home/HeroSection.jsx
@@ -9,10 +9,9 @@ const HeroSection = () => {
                 src="/bg.png"
                 alt="Hero Background"
                 layout="fill"
-                // objectFit="cover"
+                objectFit="cover"
                 quality={100}
                 priority
-                // className="h-[1358px]"
             />
 
             {/* Text Overlay */}
@@ -21,8 +20,23 @@ const HeroSection = () => {
                     <h1 className="text-[35px] font-semibold text-[#FFFBFB] ml-[374px]">Welcome to Halal Fly</h1>
                     <h1 className="ml-[336px] font-semibold text-[50px] text-[#FFF]">Discover Halal Experiences</h1>
                     <h1 className="ml-[600px] font-semibold text-[50px] text-[#FFF]">Not Just Travel</h1>
-                    <h1 className="text-[#FFF] ml-[337px] font-semibold text-[35px]">We provide Travel and Ummrah Package all over the world</h1>
+                    <h1 className="text-[#FFF] ml-[337px] font-semibold text-[35px]">
+                        We provide Travel and Umrah Package all over the world
+                    </h1>
                 </div>
+            </div>
+
+            {/* Booking Section Image */}
+            <div className="max-w-6xl mx-auto">
+                {/* <Image
+                    src="/book.png"
+                    alt="Hero Booking"
+                    width={1200}  // Adjust width as needed
+                    height={450}  // Adjust height as needed
+                    quality={100}
+                    priority
+                /> */}
+                <img src="/book.png" alt="Hero Booking" className="w-[1100px] top-[1140px] absolute"/>
             </div>
         </div>
     );
